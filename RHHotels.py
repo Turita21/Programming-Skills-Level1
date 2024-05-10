dff562a42ec011596e457398c1d0a487861d94cf
Taniana Paredes <taniana1521@gmail.com>
2024-05-10 21:17:04 +0200

    Last Changes

--- RHHotels.py
+++ RHHotels.py
@@ -40,18 +40,20 @@ def login(dictionary):
     return message
 
 def menu():
+    print("-------------------------------------------------------------------")
     print("\nWe are delighted to have our hotels all over the world.")
-    print("\nPlease choose an option from below to continue: ")
+    print("-------------------------------------------------------------------")
+    print("\nPlease choose an option from below to continue: \n")
     print("1 -Search by destination.\n")
-    print("2 -Search by date.\n")
-    print("3 -Make a reservation.\n")
-    print("4 -View my reserved rooms.\n")
-    print("5 -Log out.\n")
-    choice=input("Number of your choice :")
-    return int(choice)
+    print("2 -Make a reservation.\n")
+    print("3 -View my reserved rooms.\n")
+    print("4 -Log out.\n")
+    choice=int(input("Number of your choice :"))
+    return choice
 
 def destination():
-    print("We count with hotels in these destinations:\n")
+    print("-------------------------------------------------------------------")
+    print("\nWe count with hotels in these destinations:\n")
     print("\t 1- Spain \n")
     print("\t 2- France \n")
     print("\t 3- Portugal \n")
@@ -72,7 +74,8 @@ def destination():
         print("That number is not asssigned to any country. Please try again")
     print(f"\nYou choose {country}\n")
     if country == "Spain":
-        print("\nIn this destination we have hotels in: ")
+        print("-------------------------------------------------------------------")
+        print("\nIn this destination we have hotels in: \n")
         print("\t 1- Madrid\n")
         print("\t 2- Barcelona\n")
         print("\t 3- Valencia\n")
@@ -86,8 +89,61 @@ def destination():
         elif num_city == 3:
             city = "Valencia"
             return "\nYour destination is " + city + " " + country 
+    elif country == "France":
+        print("-------------------------------------------------------------------")
+        print("\nIn this destination we have hotels in: ")
+        print("\t 1- Paris\n")
+        print("\t 2- Marseille\n")
+        num_city = int(input("\nPlease enter the number of your city of you preference: "))
+        if num_city == 1:
+            city = "Paris"
+            return "\nYour destination is " + city + " " + country
+        elif num_city == 2:
+            city = "Marseille"
+            return "\nYour destination is " + city + " " + country 
+    elif country == "Portugal":
+        print("-------------------------------------------------------------------")
+        print("\nIn this destination we have hotels in: ")
+        print("\t 1- Madeira\n")
+        print("\t 2- Lisbon\n")
+        print("\t 3- Porto\n")
+        num_city = int(input("\nPlease enter the number of your city of you preference: "))
+        if num_city == 1:
+            city = "Madeira"
+            return "\nYour destination is " + city + " " + country
+        elif num_city == 2:
+            city = "Lisbon"
+            return "\nYour destination is " + city + " " + country 
+        elif num_city == 3:
+            city = "Porto"
+            return "\nYour destination is " + city + " " + country 
+    elif country == "Italy":
+        print("-------------------------------------------------------------------")
+        print("\nIn this destination we have hotels in: ")
+        print("\t 1- Rome\n")
+        print("\t 2- Milan\n")
+        num_city = int(input("\nPlease enter the number of your city of you preference: "))
+        if num_city == 1:
+            city = "Rome"
+            return "\nYour destination is " + city + " " + country
+        elif num_city == 2:
+            city = "Milan"
+            return "\nYour destination is " + city + " " + country 
+    elif country == "Germany":
+        print("-------------------------------------------------------------------")
+        print("\nIn this destination we have hotels in: ")
+        print("\t 1- Munich\n")
+        print("\t 2- Berlin\n")
+        num_city = int(input("\nPlease enter the number of your city of you preference: "))
+        if num_city == 1:
+            city = "Munich"
+            return "\nYour destination is " + city + " " + country
+        elif num_city == 2:
+            city = "Berlin"
+            return "\nYour destination is " + city + " " + country 
 
 def hotels():
+    print("-------------------------------------------------------------------")
     print("\nSelect the kind of room of your preference: \n")
     print("\t 1- Single Room\n")
     print("\t 2- Double Room\n")
@@ -98,24 +154,54 @@ def hotels():
     
     n = 0
     list_room = []
+    single_room = 3
+    double_room = 6
+    group_room = 6
+    vip_suite = 6
+    luxury_suite = 3
     while n < number_of_rooms: 
         room_type = int(input("\nEnter the number of the room from the list above: "))
         n += 1
         if room_type == 1:
             room = "Single room"
-            print(f"\nThe room {n} is a single room.")
+            single_room -= 1
+            if single_room == 0:
+                print("Sorry we dont have available more single rooms. ")
+                break
+            else:
+                print(f"\nThe room {n} is a single room.")
         elif room_type == 2:
             room = "Double room"
-            print(f"\nThe room {n} is a double room.")
+            double_room -= 1
+            if double_room == 0:
+                print("Sorry, we don't have available more double rooms. ")
+                break
+            else:
+                print(f"\nThe room {n} is a double room.")
         elif room_type == 3:
             room = "Group room"
-            print(f"\nThe room {n} is a Group room.")
+            group_room -= 1
+            if  group_room == 0:
+                print("Sorry. There are no more group rooms left. ")
+                break
+            else:
+                print(f"\nThe room {n} is a Group room.")
         elif room_type == 4:
             room = "VIP Suite"
-            print(f"\nThe room {n} is a VIP suite.")
+            vip_suite -= 1
+            if vip_suite == 0:
+                print("Sorry. There are no more VIP suites left.")
+                break
+            else:
+                print(f"\nThe room {n} is a VIP suite.")
         elif room_type == 5:
             room = "Luxury Suite"
-            print(f"\nThe room {n} is a Luxury suite.")
+            luxury_suite -= 1
+            if luxury_suite  == 0:
+                print("There are no more Luxury Suites left in our hotel.\n")
+                break
+            else:
+                print(f"\nThe room {n} is a Luxury suite.")
         else:
             print("This option is incorrect, please try again.")
         list_room.append(room)
@@ -127,7 +213,7 @@ def reservations():
     day =  int(input("\tDay (dd): "))
     month = int(input("\tMonth (mm): "))
     year = int(input("\tYear (yyyy): "))
-    hour = input("\tHour [hh]: ")
+    hour = int(input("\tHour [hh]: "))
     if hour > 24:
         print("You have enter an invalid hour. Please try again.\n")
         hour = input("\tHour [hh]: ")
@@ -145,7 +231,7 @@ def reservations():
                             "Year": year,
                             "Hour": hour}
         
-    return "Your reservation is set to the following datetime " +  str(date_CheckIn_reservation) + " until " + str(date_CheckOut_reservation)
+    return "\nYour reservation is set to the following datetime: \n " +  str(date_CheckIn_reservation) + " until " + str(date_CheckOut_reservation) 
         
 def prices(list_room):
     total = 0
@@ -162,20 +248,95 @@ def prices(list_room):
             price_room = 550
         total += price_room
     return total
-    
+
+def user_data():
+    name = input("\tName: ").capitalize()
+    lastname = input("\tLast Name").capitalize()
+    dni = input("\tEnter your dni/nie or passport: ")
+    mail = input("\tE-mail address: ")
+    phone = input("\tPhone number: ") 
+    dictionary = {"Name": name,"Last Name":lastname, "DNI/NIE/Passport":dni, "Mail":mail, "Phone":phone}
+    return dictionary
     
 
 print("------------------------------------------------------------------")
 print("------------------------ WELCOME TO RH HOTELS---------------------")
 
-print(prices(["VIP suite"]))
-# account = input("Do you already have an account ? Y/N: ").lower()
-# if account == 'y':
-#     d1 = {"Name":"Coral","Mail address":"cori119","Password":"hagro"}
-#     user = login(d1)
-#     print(user)
-# else:
-#     print("\t-------------Please sign up.------------")
-#     user = user_singup()
-#     print("\t--------Account created. Enter your data to login.-------")
-#     print(login(user))
+account = input("Do you already have an account ? Y/N: ").lower()
+if account == 'y':
+    d1 = {"Name":"Coral","Mail address":"cori119","Password":"hagro"}
+    user = login(d1)
+    print(user)
+    if user == "\n\tYou have succesfully logged in.":
+        menu = menu()
+        if menu == 1:
+            destiny = False
+            print(destination())
+            destiny = True
+            if destiny == True:
+                check = input("\n\n Are you shoure that the destination is correct? Enter Y or N to continue: ").upper()
+                if check == "Y":
+                    hotel = hotels()
+                    print(hotel)
+                    print(reservations())
+                    prices = prices(hotel)
+                    print("\t\t\nThe total of your reservation is : ",prices,"€.")
+                    user_data()
+                    confirmation = input("If the information is correct please enter yes/no to proceed: ").lower()
+                    if confirmation == "yes":
+                        print("Thank you for using our hotels. Have a nice stay.")
+                    elif confirmation == "no":
+                        print("\n You have been returned to the main menu. \n")
+                        print(menu())
+                else: 
+                     print("\n You have been returned to the main menu. \n")
+                     print(menu())
+        elif menu == 2:
+            print(reservations)
+        elif menu == 3:
+            def view_my_rooms():
+                pass
+        elif menu == 4:
+            print("Goodbye! See you soon!\n")
+        else:
+            print("\nInvalid number. Please try again.")
+else:
+    print("\t-------------Please sign up.------------")
+    user = user_singup()
+    print("\t--------Account created. Enter your data to login.-------")
+    print(login(user))
+    if login(user) == "\n\tYou have succesfully logged in.":
+        menu = menu()
+        if menu == 1:
+            destiny = False
+            print(destination())
+            destiny = True
+            if destiny == True:
+                check = input("\n\n Are you shoure that the destination is correct? Enter Y or N to continue: ").upper()
+                if check == "Y":
+                    hotel = hotels()
+                    print(reservations())
+                    prices = prices(hotel)
+                    print("\t\t\nThe total of your reservation is : ",prices,"€.")
+                    user_data()
+                    confirmation = input("If the information is correct please enter yes/no to proceed: ").lower()
+                    if confirmation == "yes":
+                        print("Thank you for using our hotels. Have a nice stay.")
+                    elif confirmation == "no":
+                        print("\n You have been returned to the main menu. \n")
+                        print(menu())
+                else: 
+                     print("\n You have been returned to the main menu. \n")
+                     print(menu())
+        elif menu == 2:
+            print(reservations)
+        elif menu == 3:
+            def view_my_rooms():
+                pass
+        elif menu == 4:
+            print("Goodbye! See you soon!\n")
+        else:
+            print("\nInvalid number. Please try again.")
+
+
+
